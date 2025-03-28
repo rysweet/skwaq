05451ad8f8d8ef4d6fd9a623e9544c5b0b009b29
Ryan Sweet <rysweet@microsoft.com>
2025-03-27 18:30:45 -0700

    remove this stuff

--- skwaq/code_analysis/summarization/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Code summarization package for advanced code analysis.
-
-This package provides functionality for summarizing code at different levels,
-inferring developer intent, reconstructing architecture, and linking related components.
-"""
